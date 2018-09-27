d794180edf3447b3a8ce4867c8b51d428a290ded
StijnDefour <defourstijn10@gmail.com>
2018-09-27 10:49:01 +0200

    Starting with map

--- ExamenAndroid/app/src/main/java/be/ap/edu/examenandroid/MapsActivity.java
+++ ExamenAndroid/app/src/main/java/be/ap/edu/examenandroid/MapsActivity.java
@@ -1,10 +1,21 @@
 package be.ap.edu.examenandroid;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends Activity {
 
@@ -17,7 +28,9 @@ public class MapsActivity extends Activity {
         super.onCreate(bundle);
         setContentView(R.layout.maps_layout);
 
-        TextView text = (TextView) findViewById(R.id.testText);
+        if(Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
 
         Bundle extras = getIntent().getExtras();
         if (extras == null) {
@@ -27,22 +40,77 @@ public class MapsActivity extends Activity {
         longtitude = extras.getDouble("longtitude");
         latitude = extras.getDouble("latitude");
 
-        if (longtitude != null) {
+        if (longtitude != null && latitude != null) {
 
         }
 
-        /*
-            Button button1 = (Button) findViewById(R.id.button1);
-            button1.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    intent = new Intent();
-                    intent.putExtra("returnValue1", text1.getText().toString());
-                    intent.putExtra("returnValue2", text2.getText().toString());
-                    setResult(RESULT_OK, intent);
-                    finish();
-                }
-            });
-        */
+        // Return to MainActivity
+        Button button = (Button) findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
     }
 
+    // START PERMISSION CHECK
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 1;
+
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String message = "osmdroid permissions:";
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nLocation to show user location.";
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nStorage access to store map tiles.";
+        }
+        if(!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } // else: We already have permissions, so handle as normal
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION and WRITE_EXTERNAL_STORAGE
+                Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                if(location && storage) {
+                    // All Permissions Granted
+                    Toast.makeText(MainActivity.this, "All permissions granted", Toast.LENGTH_SHORT).show();
+                }
+                else if (location) {
+                    Toast.makeText(this, "Storage permission is required to store map tiles to reduce data usage and for offline usage.", Toast.LENGTH_LONG).show();
+                }
+                else if (storage) {
+                    Toast.makeText(this, "Location permission is required to show the user's location on map.", Toast.LENGTH_LONG).show();
+                }
+                else { // !location && !storage case
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "Storage permission is required to store map tiles to reduce data usage and for offline usage." +
+                            "\nLocation permission is required to show the user's location on map.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    // END PERMISSION CHECK
 }
